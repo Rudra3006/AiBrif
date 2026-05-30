8c932be814fb4f64d881bb26efae4cd8b770cb70
Rudra3006 <114402625+Rudra3006@users.noreply.github.com>
2026-05-30 17:21:56 +0930

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AIBrif from "./aibrif.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AIBrif />
+  </React.StrictMode>
+);
